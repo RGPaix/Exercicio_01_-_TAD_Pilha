d8a0c588f12469ec8379fcf4eb9835a3eec8ec27
Rodrigo Gomes <161089642+RGPaix@users.noreply.github.com>
2025-08-28 19:14:48 -0300

    Update Main.java

--- src/Ex_04/Main.java
+++ src/Ex_04/Main.java
@@ -1,4 +1,54 @@
-package Ex_041;
+package Ex_04;
+
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma expressão em notação polonesa reversa: ");
+        String expressao = sc.nextLine();
+
+        int resultado = avaliarExpressao(expressao);
+
+        System.out.println("Resultado = " + resultado);
+
+        sc.close();
+    }
+
+    public static int avaliarExpressao(String expr) {
+        PilhaVetor<Integer> pilha = new PilhaVetor<>(expr.length());
+        String[] tokens = expr.split(" ");
+
+        for (String token : tokens) {
+            char c = token.charAt(0);
+
+            if (Character.isDigit(c)) {
+                pilha.empilhar(Integer.parseInt(token));
+            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                int b = pilha.desempilhar();
+                int a = pilha.desempilhar();
+                int resultado = 0;
+
+                switch (c) {
+                    case '+':
+                        resultado = a + b;
+                        break;
+                    case '-':
+                        resultado = a - b;
+                        break;
+                    case '*':
+                        resultado = a * b;
+                        break;
+                    case '/':
+                        resultado = a / b;
+                        break;
+                }
+
+                pilha.empilhar(resultado);
+            }
+        }
+
+        return pilha.desempilhar();
+    }
 }
